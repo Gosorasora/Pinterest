fc3d56063201a1ef5f6e98dfc47744b2c12701c3
DongHyeon Ko <hanser0204@naver.com>
2025-03-07 14:42:00 +0900

    Django corues 10

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def helloWorld(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
       return render(request, "accountapp/helloworld.html")
-=======
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-def helloWorld(request):
-  return HttpResponse("Hello World")
->>>>>>> c443512b1c8b81232a09a87d27a4dbb1acd25f16

0a647084dd8f2785c908da5613e34a727e84c411
DongHyeon Ko <hanser0204@naver.com>
2025-03-24 14:15:57 +0900

    Django course 21

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import AccountCreateView
 
 app_name = "accountapp"
 urlpatterns = [
     path('helloworld/', views.helloworld, name='helloworld'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 
 ]
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,14 @@
 from audioop import reverse
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import CreateView
+
 from accountapp.models import helloWorld
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+
 
 # Create your views here.
 
@@ -21,3 +27,9 @@ def helloworld(request):
     else:
         helloWorld_list = helloWorld.objects.all()
         return render(request, "accountapp/helloworld.html", context={"helloWorld_list": helloWorld_list})
+
+class AccountCreateView(CreateView):
+    model = User #장고 기본 제공 모델
+    form_class = UserCreationForm #장고 기본 제공 폼
+    success_url = reverse_lazy('accountapp:helloworld') #reverse 함수형 reverse_lazy 클래스형
+    template_name = 'accountapp/create.html'

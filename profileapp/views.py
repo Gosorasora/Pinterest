fcb19274c1a9818bfbb99f8525193bae419a37cd
DongHyeon Ko <hanser0204@naver.com>
2025-04-04 17:23:57 +0900

    Django courser 33

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,5 @@
+from django.urls import reverse
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
@@ -6,7 +8,7 @@ from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
-from profileapp.decoratros import profile_ownership_required
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -17,7 +19,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:helloworld')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -32,11 +33,16 @@ class ProfileCreateView(CreateView):
             temp_profile.save()
             return super().form_valid(form)
 
-@method_decorator(profile_ownership_required(), 'get')
-@method_decorator(profile_ownership_required(), 'post')
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) #self.object = user
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:helloworld')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) #self.object = user

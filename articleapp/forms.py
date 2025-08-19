7d18767393dfe772e1116c6f35cbbdd1d21eaace
DongHyeon Ko <hanser0204@naver.com>
2025-08-19 17:34:25 +0900

    Django course 44 WYSIWYG setting

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,7 +1,17 @@
 from django.forms.models import ModelForm
+from pkg_resources import require
+
 from articleapp.models import Article
+from django import forms
+
+from projectapp.models import Project
+
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editalble text=left',
+                                                           'style':'height:auto;'}))
+    project = forms.ModelChoiceField(queryset = Project.objects.all(), required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']

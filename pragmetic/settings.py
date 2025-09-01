85247dff8b1a958d94d32e741265812d452db331
DongHyeon Ko <hanser0204@naver.com>
2025-09-01 15:44:36 +0900

    fixed allowd_host

--- pragmetic/settings.py
+++ pragmetic/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '149.28.234.63']
 
 # Application definition
 

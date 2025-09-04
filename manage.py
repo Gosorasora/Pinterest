66d401ee2b6ac63de7c159effb01daf69994bb24
DongHyeon Ko <hanser0204@naver.com>
2025-09-04 20:41:57 +0900

    separate deploy & local

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pragmetic.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pragmetic.settings.local')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
     try:
